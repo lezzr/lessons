8656d13033380c6e6ba9e3339923e1dbae4b15bc
lezzr <bltko21@gmail.com>
2021-05-01 11:41:07 +1000

    up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+// Переделать в ДЗ не использовать fetch а Promise
+let getRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status !== 200) {
+          reject('Error');
+        } else {
+          resolve(xhr.responseText);
+        }
+      }
+    };
+    xhr.send();
+  });
+};
+
 const API =
   'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
