89d3ae1b8d82b64f2f8f37d32a92d64f8270c89a
lezzr <bltko21@gmail.com>
2021-06-28 16:22:24 +1000

    lesson1

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" :date="computedDate" />
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Calculator from './components/Calculator.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Calculator,
+  },
+  data() {
+    return {
+      count: 0,
+      currentDate: new Date().toDateString(),
+      message: 'hello Vue',
+    };
+  },
+  methods: {
+    onClick() {
+      this.count += 1;
+    },
+    onClick2(param1, param2) {
+      this.count = param1 + param2;
+    },
+  },
+  computed: {
+    computedDate() {
+      return this.currentDate || new Date();
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <input type="number" v-model.number="operand1" placeholder="operand1" />
+    <input type="number" v-model.number="operand2" placeholder="operand2" />
+
+    = {{ summ }}
+    <div class="keyboard">
+      <button @click="add(operand1, operand2)">+</button>
+      <button @click="subt(operand1, operand2)">-</button>
+      <button @click="mult(operand1, operand2)">*</button>
+      <button @click="divide(operand1, operand2)">/</button>
+      <button @click="powbtn(operand1, operand2)">^</button>
+      <button @click="fulldiv(operand1, operand2)">//</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Calculator',
+  data() {
+    return {
+      operand1: 0,
+      operand2: 0,
+      summ: 0,
+    };
+  },
+  watch: {
+    summ: function(newValue, oldValue) {
+      console.log(newValue, oldValue);
+    },
+  },
+  methods: {
+    add() {
+      this.summ = this.operand1 + this.operand2;
+    },
+    subt() {
+      this.summ = this.operand1 - this.operand2;
+    },
+    mult() {
+      this.summ = this.operand1 * this.operand2;
+    },
+    divide() {
+      this.summ = this.operand1 / this.operand2;
+    },
+    powbtn() {
+      this.summ = Math.pow(this.operand1, this.operand2);
+    },
+    fulldiv() {
+      this.summ = Math.floor(this.operand1 / this.operand2);
+    },
+  },
+  computed: {
+    powWithOperand() {
+      return Math.pow(this.operand1, this.operand2);
+    },
+    powWithSum() {
+      return Math.pow(this.sum, 4);
+    },
+  },
+};
+</script>
+
+<style></style>
