ff0ce4a74bd6f380a52572e8e16b16876710cae3
lezzr <bltko21@gmail.com>
2021-09-03 23:55:49 +1000

    comm

---
+++ src/store/middlewares/thunks.js
@@ -0,0 +1,9 @@
+
+export const thunk = (store) => (next) => (action) => {
+    if (typeof action === "function") {
+        action(store.dispatch, store.getState)
+        return
+    }
+
+    return next(action)
+}
